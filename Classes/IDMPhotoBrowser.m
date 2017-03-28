41d35ce5c6837d7980d2ede29aae2b2008f763f8
ldbchan <ldb.chan@gmail.com>
2017-03-28 17:31:03 +0800

    Edit move caption view to top

--- Classes/IDMPhotoBrowser.m
+++ Classes/IDMPhotoBrowser.m
@@ -1152,8 +1152,8 @@ NSLocalizedStringFromTableInBundle((key), nil, [NSBundle bundleWithPath:[[NSBund
 - (CGRect)frameForCaptionView:(IDMCaptionView *)captionView atIndex:(NSUInteger)index {
     CGRect pageFrame = [self frameForPageAtIndex:index];
 
-    CGSize captionSize = [captionView sizeThatFits:CGSizeMake(pageFrame.size.width, 0)];
-    CGRect captionFrame = CGRectMake(pageFrame.origin.x, pageFrame.size.height - captionSize.height - (_toolbar.superview?_toolbar.frame.size.height:0), pageFrame.size.width, captionSize.height);
+    CGSize captionSize = [captionView sizeThatFits:CGSizeMake(pageFrame.size.width, 24)];
+    CGRect captionFrame = CGRectMake(pageFrame.origin.x, 30, pageFrame.size.width, captionSize.height);
 
     return captionFrame;
 }
