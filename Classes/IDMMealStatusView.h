cbd21d986186a2a6e47f507b891e202bbca8cf95
result0924 <result0924@gmail.com>
2017-08-25 10:42:12 +0800

    Add IDMMealStatusView

---
+++ Classes/IDMMealStatusView.h
@@ -0,0 +1,13 @@
+//
+//  IDMMealStatusView.h
+//  PhotoBrowserDemo
+//
+//  Created by justin on 2017/8/25.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDMMealStatusView : UIView
+
+@end
---
+++ Classes/IDMMealStatusView.m
@@ -0,0 +1,21 @@
+//
+//  IDMMealStatusView.m
+//  PhotoBrowserDemo
+//
+//  Created by justin on 2017/8/25.
+//
+//
+
+#import "IDMMealStatusView.h"
+
+@implementation IDMMealStatusView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
